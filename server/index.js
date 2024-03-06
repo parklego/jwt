54ffa41b316e8d2e047b9004c474f67ef8834681
parklego <hbd9425@gmail.com>
2024-03-06 20:01:35 +0900

    jwt 생성 및 cors 에러 처리

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 import StudentModel from "./models/Student.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 mongoose.connect("mongodb://127.0.0.1:27017/school");
 
@@ -24,6 +30,51 @@ app.post("/register", (req, res) => {
   StudentModel.create({ username, password, email })
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  StudentModel.findOne({ email })
+    .then((user) => {
+      if (user) {
+        if (user.password === password) {
+          const accessToken = jwt.sign(
+            {
+              email,
+            },
+            "access-token-secret-key",
+            {
+              expiresIn: "1m",
+            }
+          );
+          const refreshToken = jwt.sign(
+            {
+              email,
+            },
+            "refresh-token-secret-key",
+            {
+              expiresIn: "5m",
+            }
+          );
+
+          res.cookie("accessToken", accessToken, {
+            maxAge: 60000,
+          });
+          res.cookie("refreshToken", refreshToken, {
+            maxAge: 300000,
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+          });
+          res.json({ login: true });
+        }
+      } else {
+        res.json({ login: false, message: "Not allowed" });
+      }
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.listen(3001, () => {
