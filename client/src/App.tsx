e824ebb64d8571c17aeb31116a203b2c03f52cf8
parklego <hbd9425@gmail.com>
2024-03-06 19:09:12 +0900

    등록페이지

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Registration from "./Registration";
 
 function App() {
   return (
     <>
-      <div>init </div>
+      <Registration />
     </>
   );
 }
---
+++ client/src/Registration.tsx
@@ -0,0 +1,95 @@
+import axios from "axios";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBInput,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { useState } from "react";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3001/register", { username, email, password })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <MDBContainer fluid>
+      <MDBCard className="text-black m-5" style={{ borderRadius: "25px" }}>
+        <MDBCardBody>
+          <MDBRow>
+            <MDBCol
+              md="10"
+              lg="6"
+              className="order-2 order-lg-1 d-flex flex-column align-items-center"
+            >
+              <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                Sign up
+              </p>
+              <form onSubmit={handleSubmit}>
+                {" "}
+                <div className="d-flex flex-row align-items-center mb-4 ">
+                  <MDBIcon fas icon="user me-3" size="lg" />
+                  <MDBInput
+                    label="Your Name"
+                    id="form1"
+                    type="text"
+                    className="w-100"
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="envelope me-3" size="lg" />
+                  <MDBInput
+                    label="Your Email"
+                    id="form2"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="lock me-3" size="lg" />
+                  <MDBInput
+                    label="Password"
+                    id="form3"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <center>
+                  <button type="submit" className="btn btn-primary btn-lg">
+                    Resister
+                  </button>
+                </center>
+              </form>
+            </MDBCol>
+
+            <MDBCol
+              md="10"
+              lg="6"
+              className="order-1 order-lg-2 d-flex align-items-center"
+            >
+              <MDBCardImage
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                fluid
+              />
+            </MDBCol>
+          </MDBRow>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBContainer>
+  );
+}
+
+export default App;
