26dac5a7f549db6865e5507c31688ccf74e0fdde
parklego <hbd9425@gmail.com>
2024-03-08 03:51:26 +0900

    home 라우터 이동 버튼 추가

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Home</h2>
+      <div>
+        <button
+          className="btn btn-primary btn-sm"
+          style={{ marginRight: "10px" }}
+          onClick={() => navigate("/register")}
+        >
+          회원가입
+        </button>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => navigate("login")}
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
